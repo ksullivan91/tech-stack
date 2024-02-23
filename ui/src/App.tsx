df3afc3b27f43ed6747730298be4cf0268bffc02
Kyle <sully8437@gmail.com>
2024-02-23 11:38:47 -0600

    add base-ui-react to stack

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
-import './App.css'
+import './App.css';
+import { Typography } from 'base-ui-react';
 
 const App: React.FC = () => {
-
   return (
     <>
-      Hello World
+      <Typography variant='h1'>Hello World</Typography>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
